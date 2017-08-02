e24edc597ea6cb93daa9d56a06aff22aa5224d85
urnorchrislee <18513468484@163.com>
2017-08-02 21:15:21 +0800

    image lazyload

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 
 import 'common/stylus/index.styl'
 
